b8a3fa37561a75e8862b9dc032bcdcb1656bfbc3
Huy phạm <huypham3062k3@gmail.com>
2023-11-01 09:11:55 +0700

    a

---
+++ src/main/java/com/vt/CrudApiStudents/CrudApiStudentsApplication.java
@@ -0,0 +1,13 @@
+package com.vt.CrudApiStudents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudApiStudentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudApiStudentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vt/CrudApiStudents/CrudApiStudentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vt.CrudApiStudents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApiStudentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
